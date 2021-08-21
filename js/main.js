e1bfb76b4c9c2f77e45b911f9b7adff47f1e43ec
Dimitar Petrov <dimitarpetrov1990@gmail.com>
2021-08-21 12:43:50 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 function goToContact() {
-    var contact = document.querySelector(".contact-me");
-    contact.scrollIntoView();
+  var contact = document.querySelector(".contact-me");
+  contact.scrollIntoView();
 }
 
 function goToWork() {
-    var work = document.querySelector(".my-work");
-    work.scrollIntoView();
+  var work = document.querySelector(".my-work");
+  work.scrollIntoView();
+}
+
+function goToCV() {
+  var cv = document.querySelector(".section-about");
+  cv.scrollIntoView();
 }
